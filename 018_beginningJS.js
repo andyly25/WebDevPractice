83f4d89c8c618eaa6a20a09e43feac271047bc34
Andy Ly <andywindsong@gmail.com>
2017-08-07 18:07:56 -0700

    Added in some comments

--- 018_beginningJS.js
+++ 018_beginningJS.js
@@ -1,4 +1,9 @@
+
+// You can use variables with var to store data.
+// prompt takes a user input
 var username = prompt("What is your name?(first and last)");
 var age = prompt("How old are you now?");
+// alert makes an annoying popup appear
 alert("Nice to meet you, " + username + " who is currently " + age);
+// this just outputs your results into the console log
 console.log("Also great to meet you, " + username + " who is currently " + age);
