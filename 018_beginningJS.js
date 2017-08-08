15c0fadf2e0ca2091d15a09225d66a0f7244918f
Andy Ly <andywindsong@gmail.com>
2017-08-07 18:05:50 -0700

    intro to JS console

---
+++ 018_beginningJS.js
@@ -0,0 +1,4 @@
+var username = prompt("What is your name?(first and last)");
+var age = prompt("How old are you now?");
+alert("Nice to meet you, " + username + " who is currently " + age);
+console.log("Also great to meet you, " + username + " who is currently " + age);
