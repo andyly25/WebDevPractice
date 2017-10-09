e4b95ea6341ee7144f1d423bff4dd0881c9f2606
Andy Ly <andywindsong@gmail.com>
2017-10-09 15:38:56 -0700

    simple api search finished

--- j043_backendFun/introToApi/movieApi/app.js
+++ j043_backendFun/introToApi/movieApi/app.js
@@ -4,12 +4,22 @@ var request = require("request");
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.send("Home page");
+    res.render("search");
 })
 
 app.get("/results", function(req, res){
-    request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+    // here we grab the value searched from home page
+    var query = req.query.searched;
+    // better to keep url inside a var to save space and look nicer
+    // forgot the instructor didnt need API key back then
+    var apiKey = "&apikey=thewdb"
+    // here we form the basic url that the api uses to grab things we need
+    var url = "http://omdbapi.com/?s=" + query + apiKey
+    // basic request format we should try remember
+    request(url, function(error, response, body){
+        // if everything works daijoubu
         if(!error && response.statusCode == 200){
+            // The JSON.parse(body) is necessary to make a JS object that we can use
             var data = JSON.parse(body)
             // Then we can narrow our search down
             // res.send(data["Search"][0]);
@@ -18,6 +28,11 @@ app.get("/results", function(req, res){
     });
 });
 
-app.listen(3000, process.env.IP, function(){
+// A simple one I found you can try using online, although the IP address depends
+// app.listen(process.PORT || 3000, process.env.IP || '192.168.178.1', function(){
+//     console.log('Movie App started on localhost:3000');
+// })
+// Simple cookie cutter, decided to google and this works
+app.listen(process.PORT || 3000, process.env.IP, function(){
     console.log('Movie App started on localhost:3000');
 })
